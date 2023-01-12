f1862bcb882737a9733ccc74ed336c6f2517a34c
Ivan Galanin <i.g.yappi@yandex.ru>
2023-01-12 22:54:16 +0300

    Some stuff added

--- knowledge.inc.php
+++ knowledge.inc.php
@@ -1,14 +1,22 @@
 <?php
-  $a = 'Мои знания на';
-  $b = 100;
-  $c = '%'; 
+    $a = 'Строка, конкатенованная из';
+    $b = 3;
+    $c = ' переменных разных типов';
 ?>
 
 <?php
-  $price = 33;
-  if($price >= 15 && $price <= 20) {
-    $d = 'Это число находится между 15 и 20';
-  } else {
-    $d = 'Это число не находится между 15 и 20';
-  }
+    $today = date('d.m.Y');
+    $curr_month = date('m');
+    $d = 'Сегодня ' . $today . ', время года: ';
+    $month_int = intval($curr_month);
+    if ($month_int <= 2 || $month_int > 11) {
+        $season = 'Зима';
+    } elseif ($month_int >= 3 && $month_int <= 5) {
+        $season = 'Весна';
+    } elseif ($month_int >= 6 && $month_int <= 8) {
+        $season = 'Лето';
+    } else {
+        $season = 'Осень';
+    }
+    $d .= $season;
 ?>
--- menu.inc.php
+++ menu.inc.php
@@ -1,9 +1,9 @@
 <nav>
     <ul>
-        <li><a href="#"> Menulink 1 </a></li>
-        <li><a href="#"> Menulink 2 </a></li>
-        <li><a href="#"> Menulink 3 </a></li>
-        <li><a href="#"> Menulink 4 </a></li>
-        <li><a href="#"> Menulink 5 </a></li>
+        <li><a href="#"> Home </a></li>
+        <li><a href="#"> Projects </a></li>
+        <li><a href="#"> Design </a></li>
+        <li><a href="#"> Utilities </a></li>
+        <li><a href="#"> Contacts </a></li>
     </ul>
 </nav>
